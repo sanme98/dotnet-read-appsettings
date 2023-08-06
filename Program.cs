03adf4c6eee8c9415a3bd754178e4102923cf0e8
sanme98 <sanme98@users.noreply.github.com>
2023-08-06 17:38:53 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+// Method 1: Get directly
+Console.WriteLine(configuration["AppSettings:Title"]);
+
+// Method 2: Get via GetSection method.
+var appSettings = configuration.GetSection("AppSettings");
+Console.WriteLine(appSettings["Version"]);
+
+// Method 3: Get via GetConnection method for connection string
+var mysqlConnString = configuration.GetConnectionString("MySql");
+Console.WriteLine($"MySQL Connection String: {mysqlConnString}");
